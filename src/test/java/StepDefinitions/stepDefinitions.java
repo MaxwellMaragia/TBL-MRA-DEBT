84b169b97f033eee8b3d48aa37ce16c5245a808b
maxipain <maxmaragia@gmail.com>
2021-04-16 10:13:02 +0300

    MRA audit module scripts

---
+++ src/test/java/StepDefinitions/stepDefinitions.java
@@ -0,0 +1,189 @@
+package StepDefinitions;
+
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import gherkin.lexer.Th;
+import io.cucumber.java.DataTableType;
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import Utils.BaseClass;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+
+
+
+
+@RunWith(Cucumber.class)
+public class stepDefinitions extends BaseClass  {
+    public Properties Pro;
+    public WebDriverWait five;
+    public WebDriverWait ten ;
+    public WebDriverWait fifteen;
+    public WebDriverWait twenty ;
+    public WebDriverWait twentyfive;
+    public WebDriverWait thirty;
+    public WebDriverWait thirtyfive;
+    public WebDriverWait fourty;
+    public WebDriverWait fourtyfive;
+    public WebDriverWait fifty;
+    public WebDriverWait fiftyfive;
+    public WebDriverWait sixty;
+    public WebDriverWait sixtyfive;
+    public WebDriverWait seventy;
+    public WebDriverWait seventyfive;
+    public WebDriverWait eighty;
+    public WebDriverWait eightyfive;
+    public WebDriverWait ninety;
+    public WebDriverWait ninetyfive;
+    public WebDriverWait onehundred;
+    public WebDriverWait twohundred;
+
+
+
+    public static sharedatastep sharedata;
+
+    public stepDefinitions(sharedatastep sharedata) {
+
+        stepDefinitions.sharedata = sharedata;
+
+    }
+
+    @Before(order = 2)
+    public void method1() throws Exception {
+        Pro = new Properties();
+        FileInputStream fls = new FileInputStream("src\\test\\resources\\global.properties");
+        Pro.load(fls);
+        driver = BaseClass.getDriver();
+        five = new WebDriverWait(driver, 5);
+        ten = new WebDriverWait(driver, 10);
+        fifteen = new WebDriverWait(driver, 15);
+        twenty = new WebDriverWait(driver, 20);
+        twentyfive = new WebDriverWait(driver, 25);
+        thirty = new WebDriverWait(driver, 30);
+        thirtyfive = new WebDriverWait(driver, 35);
+        fourty = new WebDriverWait(driver, 40);
+        fourtyfive = new WebDriverWait(driver, 45);
+        fifty = new WebDriverWait(driver, 50);
+        fiftyfive = new WebDriverWait(driver, 55);
+        sixty = new WebDriverWait(driver, 60);
+        sixtyfive = new WebDriverWait(driver, 65);
+        seventy = new WebDriverWait(driver, 70);
+        seventyfive = new WebDriverWait(driver, 75);
+        eighty = new WebDriverWait(driver, 80);
+        eightyfive = new WebDriverWait(driver, 85);
+        ninety = new WebDriverWait(driver, 90);
+        ninetyfive = new WebDriverWait(driver, 95);
+        onehundred = new WebDriverWait(driver, 100);
+        twohundred = new WebDriverWait(driver, 200);
+
+    }
+
+    @Given("^User navigates to the login page$")
+    public void user_navigates_to_the_login_page() throws Throwable {
+
+//    	intergtation link for backoffice
+//    	driver.get("http://18.202.88.7:8001/trips-ui/faces/login/tripsLogin.xhtml");
+
+        //    	SIT link for backoffice
+        driver.get("https://backoffice.mra.mw:8443/trips-ui/faces/login/tripsLogin.xhtml");
+    }
+
+    @When("^Enters the username \"([^\"]*)\" and password \"([^\"]*)\" to login$")
+    public void enters_the_username_something_and_password_something_to_login(String strArg1, String strArg2) throws Throwable {
+        driver.findElement(By.id("loginForm:username")).sendKeys(strArg1);
+        driver.findElement(By.id("loginForm:password")).sendKeys(strArg2);
+        driver.findElement(By.xpath("//*[@id=\"loginForm:j_idt19\"]/span")).click();
+    }
+
+    @Then("^User should be logged in$")
+    public void user_should_be_logged_in() throws Throwable {
+        String URL = driver.getCurrentUrl();
+
+//    	Assert.assertEquals(URL, "http://18.202.88.7:8001/trips-ui/faces/login/Welcome.xhtml" );
+        Assert.assertEquals(URL, "https://backoffice.mra.mw:8443/trips-ui/faces/login/Welcome.xhtml");
+    }
+
+    @Then("^User logs out successfully$")
+    public void user_logs_out_successfully() throws Throwable {
+        driver.findElement(By.id("Logout")).click();
+    }
+
+    //---------------------------------------------------------------------Verify the Process of Assign Audit Case-----------------------------------------------------------------------------------------------//
+    @Given("^Open CRM URL Module as \"([^\"]*)\"$")
+    public void open_crm_url_module_as_something(String strArg1) throws Throwable {
+        driver = getDriver();
+        driver.get("https://" + strArg1 + ":Passw0rd@trips-crm.mra.mw:5555/TripsWorkflow/main.aspx");
+    }
+
+    @And("^Close Popup Window$")
+    public void close_Popup_Window() throws Throwable {
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement specificframe = (driver.findElement(By.id(Pro.getProperty("CRM_ExploreCrmWindow_Frame__ID"))));
+        driver.switchTo().frame(specificframe);
+        WebDriverWait CloseWindow = new WebDriverWait(driver, 60);
+        CloseWindow.until(ExpectedConditions.elementToBeClickable(By.id(Pro.getProperty("CRM_ExploreCrmWindow_Frame_Close_ID")))).click();
+    }
+
+    @And("^Click on Case management dropdown$")
+    public void click_on_case_management_dropdown() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"TabCS\"]/a/span")).click();
+    }
+
+
+    @Then("^switch to frame0$")
+    public void switch_to_frame0() throws Throwable {
+        driver.switchTo().defaultContent();
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        WebElement specificframe = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(Pro.getProperty("NextStage_Frame_ID"))));
+        driver.switchTo().frame(specificframe);
+        Thread.sleep(3000);
+
+    }
+
+    @Then("^switch to frame1$")
+    public void switch_to_frame1() throws Throwable {
+        driver.switchTo().defaultContent();
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        WebElement specificframe = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(Pro.getProperty("NextStage_Frame_ID1"))));
+        driver.switchTo().frame(specificframe);
+        Thread.sleep(3000);
+
+    }
+
+}
+
+
+

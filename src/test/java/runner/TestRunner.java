17a190126498ffbc2ad69ab86f7b50dce2960a7d
maxipain <maxmaragia@gmail.com>
2021-05-11 10:46:59 +0300

    Changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -17,7 +17,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		features = "src\\test\\resources\\FeatureFiles",
 		glue = "StepDefinitions" ,
-		tags = "@UAT_M8-08-06-06",
+		//tags = "@UAT_M8-08-07-07",
 		dryRun = false,    //checks whether each feature has a mapped step definition
 		monochrome = true,// neat output after tc run
 		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","json:target/positive/cucumber.json", "pretty", "html:target/positive/cucumber.html"}
